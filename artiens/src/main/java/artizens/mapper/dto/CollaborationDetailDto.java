65878c66d08f64964b82e4f2bb840de8d7f92b7a
practiceHard <87794574+practiceHard@users.noreply.github.com>
2021-12-15 11:17:14 +0900

    콜라보메인 수정, 상세화면 작성ing~

---
+++ artiens/src/main/java/artizens/mapper/dto/CollaborationDetailDto.java
@@ -0,0 +1,16 @@
+package artizens.mapper.dto;
+
+public class CollaborationDetailDto {
+	
+	private Long id;
+	private String title;
+	private String registerDate;
+	private String creatorNickname;
+	private String collaborationImgName;
+	private String creatorImgName;
+	private String content;
+	private Integer commentCount;
+	private Long commentId;
+	private String comment;
+
+}
