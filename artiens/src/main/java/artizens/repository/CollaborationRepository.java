fe3867a5f797f9b79c3dae8c9b039bd1d0d7b678
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2021-12-22 18:26:21 +0900

    지움

--- artiens/src/main/java/artizens/repository/CollaborationRepository.java
+++ artiens/src/main/java/artizens/repository/CollaborationRepository.java
@@ -2,7 +2,6 @@ package artizens.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import artizens.domain.Collaboration;
-import artizens.mapper.CollaborateMapper;
 import artizens.mapper.CollaborationMapper;
 public interface CollaborationRepository extends JpaRepository<Collaboration, Long>{
 }
